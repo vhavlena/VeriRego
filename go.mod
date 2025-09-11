127c6c1c4dbef8c8ca8dcd48eead617cbc5326b8
vhavlena <vohavlena@gmail.com>
2025-09-11 13:26:48 +0200

    basic test with z3 binding

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
+	github.com/vhavlena/z3-go v0.0.1 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ pkg/smt/translator_z3_test.go
@@ -0,0 +1,89 @@
+package smt
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/vhavlena/verirego/pkg/types"
+	z3 "github.com/vhavlena/z3-go/z3"
+)
+
+// TestTranslateAndCheckZ3 translates a tiny Rego module to SMT-LIB and checks SAT with Z3.
+//
+// Build/run with Z3 available:
+//
+//	go test -tags z3 ./...
+func TestTranslateAndCheckZ3(t *testing.T) {
+	// Simple, satisfiable module: p = x with body x == 1
+	rego := `
+package test
+
+p = x {
+  x == 1
+}
+`
+
+	mod, err := ast.ParseModule("test.rego", rego)
+	if err != nil {
+		t.Fatalf("failed to parse rego: %v", err)
+	}
+
+	// Minimal type info for variables used in the rule
+	ta := &types.TypeAnalyzer{
+		Types: map[string]types.RegoTypeDef{
+			"x": types.NewAtomicType(types.AtomicInt),
+			"p": types.NewAtomicType(types.AtomicInt),
+		},
+		Refs: map[string]ast.Value{
+			"x": ast.Var("x"),
+			"p": ast.Var("p"),
+		},
+	}
+
+	tr := NewTranslator(ta, mod)
+
+	// Declare only the variables we actually use (avoid schema auto-includes)
+	used := map[string]any{"x": struct{}{}, "p": struct{}{}}
+
+	// Generate type and var declarations
+	if bucket, err := tr.TypeTrans.GenerateTypeDecls(used); err != nil {
+		t.Fatalf("type decls error: %v", err)
+	} else {
+		tr.AppendBucket(bucket)
+	}
+	if bucket, err := tr.TypeTrans.GenerateVarDecls(used); err != nil {
+		t.Fatalf("var decls error: %v", err)
+	} else {
+		tr.AppendBucket(bucket)
+	}
+
+	// Translate rules to asserts
+	if err := tr.TranslateModuleToSmt(); err != nil {
+		t.Fatalf("TranslateModuleToSmt error: %v", err)
+	}
+
+	smt := strings.Join(tr.SmtLines(), "\n")
+
+	fmt.Printf("Generated SMT-LIB:\n%s\n", smt) // For debugging
+
+	// Z3: assert SMT-LIB2 string and check SAT
+	ctx := z3.NewContext(nil)
+	defer ctx.Close()
+	solver := ctx.NewSolver()
+	defer solver.Close()
+
+	if err := solver.AssertSMTLIB2String(smt); err != nil {
+		t.Fatalf("z3 AssertSMTLIB2String error: %v", err)
+	}
+	res, err := solver.Check()
+	if err != nil {
+		t.Fatalf("z3 Check error: %v", err)
+	}
+
+	// Expect the formula to be satisfiable
+	if res != z3.Sat {
+		t.Fatalf("expected SAT, got %v", res)
+	}
+}
