e46c6c3f617632ce140bbabfdc3302c830ba00ce
Jan Hranicka <hranickajan@seznam.cz>
2025-11-21 09:44:35 +0100

    feat: add general datatype for all SMT variables

--- pkg/smt/rules.go
+++ pkg/smt/rules.go
@@ -7,18 +7,6 @@ import (
 	"github.com/open-policy-agent/opa/v1/ast"
 )
 
-// RuleToSmt converts a Rego rule to an SMT-LIB assertion and appends it to the Translator's smtLines.
-//
-// Parameters:
-//
-//	rule *ast.Rule: The Rego rule to convert.
-//
-// Returns:
-//
-//	error: An error if the rule cannot be converted.
-//
-// The rule variable (rule.Head.Name) is equal to the rule value (rule.Head.Value) if and only if all body expressions hold.
-// The assertion is of the form: (assert (<=> (= ruleVar ruleValue) (and bodyExpr1 ... bodyExprN)))
 // ruleHeadToSmt converts the head of a Rego rule to its SMT-LIB representation (including the operator).
 //
 // Parameters:
@@ -72,13 +60,15 @@ func (t *Translator) ruleHeadToSmt(rule *ast.Rule, exprTrans *ExprTranslator) (s
 //
 //	error: An error if the rule cannot be converted.
 //
-// The rule variable (rule.Head.Name) is equal to the rule value (rule.Head.Value) if and only if all body expressions hold.
-// The assertion is of the form: (assert (<=> (= ruleVar ruleValue) (and bodyExpr1 ... bodyExprN)))
-func (t *Translator) RuleToSmt(rule *ast.Rule) error {
+// The rule variable (rule.Head.Name) is equal to the rule value (rule.Head.Value) if and only if all body expressions hold. It is equal to the else value otherwise (or Undef, if no else branch is suitable)
+func (t *Translator) RuleToSmt(rule *ast.Rule) (string,error) {
+	if rule == nil {
+		return "undefined", nil	// FIXME: return correct SMT undefined value
+	}
 	exprTrans := NewExprTranslatorWithVarMap(t.TypeTrans, t.VarMap)
 	smtHead, err := t.ruleHeadToSmt(rule, exprTrans)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	// Convert all body expressions to SMT
@@ -86,7 +76,7 @@ func (t *Translator) RuleToSmt(rule *ast.Rule) error {
 	for _, expr := range rule.Body {
 		smtStr, err := exprTrans.ExprToSmt(expr)
 		if err != nil {
-			return fmt.Errorf("failed to convert rule body expr: %w", err)
+			return "", fmt.Errorf("failed to convert rule body expr: %w", err)
 		}
 		bodySmts = append(bodySmts, smtStr)
 	}
@@ -102,7 +92,119 @@ func (t *Translator) RuleToSmt(rule *ast.Rule) error {
 		bodySmt = fmt.Sprintf("(and %s)", strings.Join(bodySmts, " "))
 	}
 
-	assertion := fmt.Sprintf("(assert (= %s %s))", smtHead, bodySmt)
-	t.smtAsserts = append(t.smtAsserts, assertion)
+	elseValue, err := t.RuleToSmt(rule.Else)
+	if err != nil {
+		return "", err
+	}
+	smt := fmt.Sprintf("(ite %s %s %s)", bodySmt, smtHead, elseValue)
+	return smt, nil
+}
+
+// ParametricRuleToSmt converts a parametric Rego rule to an SMT-LIB representation. 
+// The output is expected to be appended to smtLines as a function definition.
+//
+// Parameters:
+//
+//	rule *ast.Rule: The Rego rule to convert.
+//
+// Returns:
+//
+//	smt string: The SMT-LIB representation of rule.
+//	error: An error if the rule cannot be converted.
+//
+// The rule value is returned iff all body expression hold. If not, its else value is returned.
+// For empty else value, the Undef value is returned.
+func (t *Translator) ParametricRuleToSmt(rule *ast.Rule) (string,error) {
+	if rule == nil {
+		return "undefined", nil	// FIXME: return correct SMT undefined value
+	}
+	exprTrans := NewExprTranslatorWithVarMap(t.TypeTrans, t.VarMap)
+	// Get head value
+	var smtValue string
+	if rule.Head.Value == nil {
+		if rule.Head.Key != nil && rule.Head.Reference != nil {
+			var err error
+			smtValue, err = exprTrans.refToSmt(rule.Head.Reference)
+			if err != nil {
+				return "", fmt.Errorf("failed to convert rule head reference: %w", err)
+			}
+		}
+		return "", fmt.Errorf("rule head value is nil for %s", rule.Head.String())
+	} else {
+		var err error
+		smtValue, err = exprTrans.termToSmt(rule.Head.Value)
+		if err != nil {
+			return "", fmt.Errorf("failed to convert rule head value: %w", err)
+		}
+	}
+
+	// Convert all body expressions to SMT
+	bodySmts := make([]string, 0, len(rule.Body))
+	for _, expr := range rule.Body {
+		smtStr, err := exprTrans.ExprToSmt(expr)
+		if err != nil {
+			return "", fmt.Errorf("failed to convert rule body expr: %w", err)
+		}
+		bodySmts = append(bodySmts, smtStr)
+	}
+	t.AddTransContext(exprTrans.GetTransContext())
+
+	var bodySmt string
+	switch len(bodySmts) {
+	case 0:
+		bodySmt = "true"
+	case 1:
+		bodySmt = bodySmts[0]
+	default:
+		bodySmt = fmt.Sprintf("(and %s)", strings.Join(bodySmts, " "))
+	}
+
+	elseValue, err := t.ParametricRuleToSmt(rule.Else)
+	if err != nil {
+		return "", err
+	}
+	smt := fmt.Sprintf("(ite %s %s %s)", bodySmt, smtValue, elseValue)
+	return smt, nil
+}
+
+// RuleToAssert converts a Rego rule to an SMT-LIB assertion and appends it to smtLines. 
+//
+// Parameters:
+//
+//	rule *ast.Rule: The Rego rule to convert.
+//
+// Returns:
+//
+//	error: An error if the rule cannot be converted.
+//
+// For parametric rules, the assertion is a function definition (appended to smtDecls)
+// Otherwise, it is an assignment (appended to smtAsserts)
+func (t *Translator) RuleToAssert(rule *ast.Rule) error {
+	if rule.Head.Args == nil {
+		smt, err := t.RuleToSmt(rule)
+		if err != nil {
+			return err
+		}
+		assertion := fmt.Sprintf("(assert %s)", smt)
+		t.smtAsserts = append(t.smtAsserts, assertion)
+		return nil
+	}
+	// Parametric rule
+	funcName := rule.Head.Name.String()
+	a := rule.Head.Args
+	args := make([]string, 0, len(a))
+	for _, t := range a {
+		args = append(args, "(" + t.String() + " type)")	// FIXME
+	}
+	argList := "(" + strings.Join(args, " ") + ")"
+	println("argList: ", argList)
+	retType := "type"	// FIXME
+	
+	funcBody, err := t.ParametricRuleToSmt(rule)
+	if err != nil {
+		return err
+	}
+	funcDef := fmt.Sprintf("(define-func %s %s %s %s)", funcName, argList, retType, funcBody)
+	t.smtDecls = append(t.smtDecls, funcDef)
 	return nil
 }
