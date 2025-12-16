0570ba71768d4656d4312b0997908099b93cf5d8
Jan Hranicka <hranickajan@seznam.cz>
2025-12-16 09:06:57 +0100

    create: module for generating SMT expressions

---
+++ pkg/smt/factory/factory.go
@@ -0,0 +1,40 @@
+package factory
+
+import (
+	"fmt"
+	"strings"
+)
+
+func And(clauses []string) string {
+	return fmt.Sprintf("(and %s)", strings.Join(clauses, " "))
+}
+
+func Eq(v1 *string, v2 *string) string {
+	return fmt.Sprintf("(= %s %s)", *v1, *v2)
+}
+
+func Ite(ifSmt *string, thenSmt *string, elseSmt *string) string {
+	return fmt.Sprintf("(ite %s %s %s)", *ifSmt, *thenSmt, *elseSmt)
+}
+
+func Let(defSmt *string, exprSmt *string) string {
+	return fmt.Sprintf("(let %s %s)", *defSmt, *exprSmt)
+}
+
+func Assert(exprSmt *string) string {
+	return fmt.Sprintf("(assert %s)", *exprSmt)
+}
+
+func DefineFunc(funcName *string, argNames []string, bodySmt *string) string {
+	args := strings.Join(argNames, " " + VarType())
+	return fmt.Sprintf("(define-func %s (%s) %s %s)",
+		*funcName,			// function name
+		args,
+		VarType(),	// return type
+		*bodySmt)
+}
+
+func VarType() string {
+	return "OType"
+}
+
