357b1e24da8e765f7b4853e3cff53d555a58a8c9
vhavlena <vohavlena@gmail.com>
2026-02-05 16:04:19 +0100

    refactor: remove conditional skip for GitHub Actions in Z3 tests

--- pkg/smt/type_translator_z3_object_test.go
+++ pkg/smt/type_translator_z3_object_test.go
@@ -186,9 +186,6 @@ func Test_getSmtObjectConstrStore_VariousObjects_Z3(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if runningInGitHubActions() && tt.skipInActions {
-				t.Skip("skipping heavy/flaky Z3 test on GitHub Actions")
-			}
 			t.Parallel()
 
 			tr := buildTypeTranslator(t, map[string]types.RegoTypeDef{"x": tt.obj})
